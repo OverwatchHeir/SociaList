c26a7672d4177eef72ec1d376f5f656e5f3638fd
carlosdeantonio <charliedeantonio@gmail.com>
2019-05-11 18:12:11 +0200

    fixed entry points issues

--- setup.py
+++ setup.py
@@ -7,31 +7,35 @@
 from codecs import open
 from os.path import abspath, dirname, join
 from setuptools import setup
-from cli import Cli
 
-cli = Cli()
 this_dir = abspath(dirname(__file__))
 with open(join(this_dir, 'README.md'), encoding='utf-8') as file:
     long_description = file.read()
 
 setup(
     name='socialist',
-    version=cli.version,
-    long_description = long_description,
+    version='0.1.0',
+    long_description=long_description,
     url='https://github.com/OverwatchHeir/SociaList',
     download_url='https://github.com/OverwatchHeir/SociaList/archive/master.zip',
     author='OverwatchHeir',
     author_email='softw.dev@protonmail.com',
-    install_requires=['tdqm', 'phonenumbers', 'validate_email', 'py3dns'],
-    classifiers = [
+    license="GNU",
+    install_requires=[
+        'tqdm',
+        'phonenumbers',
+        'validate_email',
+        'py3dns'
+    ],
+    classifiers=[
         'Topic :: Utilities',
         'Natural Language :: English',
         'Programming Language :: Python :: 3.7'
     ],
+    packages=['socialist'],
     entry_points={
-        'console_scripts': [
-            'socialist=socialist:main',
-        ],
-    },
+        'console_scripts': ['socialist = socialist.socialist:main']
+    }
+
 
 )
